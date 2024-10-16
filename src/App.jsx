b5489757e4263e7a02c0ff6e35b88f62bf651965
StevenMadali <stevenmadali17@gmail.com>
2024-10-16 22:20:56 +0800

    useState application on button

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [result, setResult] = useState('');
+
+  const clickHandler = (event) => {
+    setResult(result.concat(event.target.value));
+  };
 
   return (
     <>
      <div className="calc">
-        <input type="text" placeholder='0' id='answer'/>
-        <input type="button" value="9" className='button'/>
-        <input type="button" value="8" className='button'/>
-        <input type="button" value="7" className='button'/>
-        <input type="button" value="6" className='button'/>
-        <input type="button" value="5" className='button'/>
-        <input type="button" value="4" className='button'/>
-        <input type="button" value="3" className='button'/>
-        <input type="button" value="2" className='button'/>
-        <input type="button" value="1" className='button'/>
+        <input type="text" placeholder='0' id='answer' value={result}/>
+        <input type="button" value="9" className='button' onClick={clickHandler}/>
+        <input type="button" value="8" className='button' onClick={clickHandler}/>
+        <input type="button" value="7" className='button' onClick={clickHandler}/>
+        <input type="button" value="6" className='button' onClick={clickHandler}/>
+        <input type="button" value="5" className='button' onClick={clickHandler}/>
+        <input type="button" value="4" className='button' onClick={clickHandler}/>
+        <input type="button" value="3" className='button' onClick={clickHandler}/>
+        <input type="button" value="2" className='button' onClick={clickHandler}/>
+        <input type="button" value="1" className='button' onClick={clickHandler}/>
 
-        <input type="button" value="0" className='button'/>
-        <input type="button" value="+" className='button'/>
-        <input type="button" value="-" className='button'/>
-        <input type="button" value="*" className='button'/>
-        <input type="button" value="/" className='button'/>
-        <input type="button" value="%" className='button'/>
-        <input type="button" value="." className='button'/>
+        <input type="button" value="0" className='button' onClick={clickHandler}/>
+        <input type="button" value="+" className='button' onClick={clickHandler}/>
+        <input type="button" value="-" className='button' onClick={clickHandler}/>
+        <input type="button" value="*" className='button' onClick={clickHandler}/>
+        <input type="button" value="/" className='button' onClick={clickHandler}/>
+        <input type="button" value="%" className='button' onClick={clickHandler}/>
+        <input type="button" value="." className='button' onClick={clickHandler}/>
 
-        <input type="button" value="Clear" className='button button1'/>
-        <input type="button" value="=" className='button button1'/>
+        <input type="button" value="Clear" className='button button1' onClick={clickHandler}/>
+        <input type="button" value="=" className='button button1' onClick={clickHandler}/>
      </div>
     </>
   )
